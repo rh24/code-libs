56eb4977cd677e0a3d94d98efeaf67fe4155757c
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-09-24 15:48:47 -0700

    Test out new game nested route

--- server.js
+++ server.js
@@ -28,6 +28,26 @@ app.get('/libs', (req, res) => {
   res.render('index');
 });
 
+app.get('/libs/:id/games/new', (req, res) => {
+  const SQL = `SELECT * FROM templates WHERE id = $1`;
+  const values = [req.params.id];
+
+  client.query(SQL, values, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.render('pages/error', { err });
+    } else {
+      res.render('pages/templates/show', {
+        template: result.rows[0]
+      });
+    }
+  });
+});
+
+app.get('/libs/:id/games/:id', (req, res) => {
+
+});
+
 app.get('*', (req, res, next) => {
   const err = new Error(`the route ${req.originalUrl} does not exist.`);
   err.statusCode = 404;
