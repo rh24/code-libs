fba6c2c0f5adb387d44cdfd3a081dc0273730fce
richjimdev <richjimdev@gmail.com>
2018-09-25 12:13:09 -0700

    desktop nav chosen, mobile nav begging to implement

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('js linked');
+$('.drop-down').on('mouseenter', () => {
+  $('.play-drop-down').show();
+})
+
+$('.play-drop-down').on('mouseleave', function () {
+  $(this).fadeOut(100);
+});
+
+// $('.drop-down')
