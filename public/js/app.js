543fa04a53dcc3b616e2be77b2d38eb743e72e5f
richjimdev <richjimdev@gmail.com>
2018-09-25 16:03:03 -0700

    small styles, logo edit, favicon

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,8 @@ $('.drop-down').on('mouseenter', () => {
   $('.play-drop-down').slideDown(200);
 })
 
-$('.play-drop-down').on('mouseleave', function () {
-  $(this).slideUp(200);
+$('.drop-down-group').on('mouseleave', function () {
+  $('.play-drop-down').slideUp(200);
 });
 
 $('#mobile-play').on('click', function () {
